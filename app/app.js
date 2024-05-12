704fd43f057287966d5e9e2186b5e515fb8d32ca
hyunsoo0922 <hskang9200@gmail.com>
2024-05-12 15:37:33 +0900

    v5

--- app/app.js
+++ app/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://dreamgreentour.netlify.app/",
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
     optionsSuccessStatus: 200,
